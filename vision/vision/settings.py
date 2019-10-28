7a42571b022a8ee71ef8c04ffc7826c0273eafff
PieroH <piero.herbec@gmail.com>
2019-10-28 14:56:55 +0000

    Profile Page

--- vision/vision/settings.py
+++ vision/vision/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 
 # specifying media folder path for event / staff pictures
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = ( os.path.join('static'),)
 
