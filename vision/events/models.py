6d23fc88a0927addebdc673bac1dbe2f1bb1d53a
PieroH <piero.herbec@gmail.com>
2019-10-28 15:42:36 +0000

    Event_pics db

--- vision/events/models.py
+++ vision/events/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 
 
 # Learn to manipulate models here https://docs.djangoproject.com/en/2.2/intro/tutorial02/
@@ -13,9 +14,19 @@ class Event(models.Model):
     description = models.TextField(null=False, verbose_name='Description')
     location = models.CharField(max_length=100, null=False)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
+    image = models.ImageField(default='imagePlaceholder.jpg', upload_to='event_pics')
+
 
     def was_published_recently(self):
         return self.date_published >= timezone.now() - datetime.timedelta(days=1)
 
     def get_absolute_url(self):
         return reverse('event_detail', kwargs={'pk':self.pk})
+
+    def save(self, **kwargs):
+        super().save(**kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
